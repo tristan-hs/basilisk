c43b918abbd1afe1937f51d23925f2c2fcf362c5
Tristan Hanson <chaoscreative1@gmail.com>
2021-03-31 15:21:30 -0600

    base roguelike

---
+++ entity_factories.py
@@ -0,0 +1,56 @@
+from components.ai import HostileEnemy
+from components import consumable
+from components.fighter import Fighter
+from components.inventory import Inventory
+from entity import Actor, Item
+ 
+player = Actor(
+    char="@",
+    color=(255, 255, 255),
+    name="Player",
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=30, defense=2, power=5),
+    inventory=Inventory(capacity=26),
+)
+ 
+orc = Actor(
+    char="o",
+    color=(63, 127, 63),
+    name="Orc",
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=10, defense=0, power=3),
+    inventory=Inventory(capacity=0),
+)
+troll = Actor(
+    char="T",
+    color=(0, 127, 0),
+    name="Troll",
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=16, defense=1, power=4),
+    inventory=Inventory(capacity=0),
+)
+
+confusion_scroll = Item(
+    char="~",
+    color=(207, 63, 255),
+    name="Confusion Scroll",
+    consumable=consumable.ConfusionConsumable(number_of_turns=10),
+)
+fireball_scroll = Item(
+    char="~",
+    color=(255, 0, 0),
+    name="Fireball Scroll",
+    consumable=consumable.FireballDamageConsumable(damage=12, radius=3),
+)
+health_potion = Item(
+    char="!",
+    color=(127, 0, 255),
+    name="Health Potion",
+    consumable=consumable.HealingConsumable(amount=4),
+)
+lightning_scroll = Item(
+    char="~",
+    color=(255, 255, 0),
+    name="Lightning Scroll",
+    consumable=consumable.LightningDamageConsumable(damage=20, maximum_range=5),
+)
